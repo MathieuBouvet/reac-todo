fe2cb7bc68ee1dde316e100e3f414c17eb085e27
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 19:52:57 +0200

    Render TodoList component in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+
+import TodoList from "../../components/TodoList";
+
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"> Todo </header>
-      <main className="App-body"></main>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header"> Todo </header>
+			<main className="App-body">
+				<TodoList />
+			</main>
+		</div>
+	);
 }
 
 export default App;
