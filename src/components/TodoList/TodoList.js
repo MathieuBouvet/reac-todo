17788df101f6fe9781ba123c7eba6b84f7bbec62
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 19:52:23 +0200

    Display the react logo on loading todos list

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./TodoList.css";
+import logo from "../../assets/images/logo.svg";
+
 class TodoList extends React.Component {
 	static propTypes = {
 		todos: PropTypes.arrayOf(
@@ -21,7 +24,14 @@ class TodoList extends React.Component {
 	}
 
 	render() {
-		return <div className="todo-list"></div>;
+		return !this.state.todosLoaded ? (
+			<div className="loading">
+				<img className="loading-logo" src={logo} alt="loading" />
+				<p> Loading...</p>
+			</div>
+		) : (
+			<div className="loaded"></div>
+		);
 	}
 }
 
