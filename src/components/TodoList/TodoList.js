dc3bb089a68a62fb661eef2ba2af67ffbb2d8440
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 19:32:38 +0200

    Add proptypes for TodoList component

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,7 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class TodoList extends React.Component {
-	static propTypes = {};
+	static propTypes = {
+		todos: PropTypes.arrayOf(
+			PropTypes.shape({
+				_id: PropTypes.number.isRequired,
+				title: PropTypes.string.isRequired,
+				done: PropTypes.bool.isRequired,
+			}).isRequired
+		),
+		todosLoaded: PropTypes.bool,
+	};
 	constructor(props) {
 		super(props);
 		this.state = {
