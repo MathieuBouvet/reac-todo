2cab8ac80e3bfcae2154d6e277096b17bcf62f3f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 19:04:48 +0200

    Create a todo mock up

---
+++ src/utils/mockTodo.js
@@ -0,0 +1,24 @@
+const todos = [
+	{
+		_id: 12,
+		text: "Sortir les poubelles",
+		done: false,
+	},
+	{
+		_id: 121,
+		text: "Nourrir le chat",
+		done: false,
+	},
+	{
+		_id: 122,
+		text: "Finnir la todo-list",
+		done: false,
+	},
+	{
+		_id: 123,
+		text: "Accomplir une tache",
+		done: false,
+	},
+];
+
+export default todos;
