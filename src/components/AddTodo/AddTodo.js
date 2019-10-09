b94f4bf7db39ba23ac693aa2b232fe17121e30e3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 20:14:21 +0200

    Create base AddTodo component

---
+++ src/components/AddTodo/AddTodo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./AddTodo.css";
+
+const AddTodo = (props) => <p></p>;
+
+AddTodo.propTypes = {};
+
+export default AddTodo;
