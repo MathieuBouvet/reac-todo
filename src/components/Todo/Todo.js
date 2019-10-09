c7147ed44cf115383beb4fd2877c63201ffb2035
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 20:03:45 +0200

    Create base Todo component

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./Todo.css";
+
+const Todo = (props) => <div className="todo"></div>;
+
+Todo.propTypes = {};
+
+export default Todo;
