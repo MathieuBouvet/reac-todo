b4fff33dd26c8000994c523688c22ff6add0b2d7
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 18:41:09 +0200

    Start todo-list project

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> Todo </header>
+      <main className="App-body"></main>
+    </div>
+  );
+}
+
+export default App;
