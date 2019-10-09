592e6f3dc79bf287394daba39faeb55ccb8f074f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-09 20:17:55 +0200

    Add tick button in Todo component

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -3,7 +3,12 @@ import PropTypes from "prop-types";
 
 import "./Todo.css";
 
-const Todo = ({ text }) => <li className="todo">{text}</li>;
+const Todo = ({ text, done }) => (
+	<li className="todo">
+		{text}
+		{!done && <button>✓</button>}
+	</li>
+);
 Todo.propTypes = {
 	text: PropTypes.string.isRequired,
 	done: PropTypes.bool.isRequired,
