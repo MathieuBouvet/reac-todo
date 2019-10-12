6dea15ea3b90d01221734149598cdf46c90c4bd5
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-12 15:48:09 +0200

    Delete prop types for TodoList
    I mixed up things...

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import "./TodoList.css";
 import logo from "../../assets/images/logo.svg";
@@ -9,16 +8,6 @@ import Todo from "../Todo";
 import data from "../../utils/mockTodo";
 
 class TodoList extends React.Component {
-	static propTypes = {
-		todos: PropTypes.arrayOf(
-			PropTypes.shape({
-				_id: PropTypes.number.isRequired,
-				title: PropTypes.string.isRequired,
-				done: PropTypes.bool.isRequired,
-			}).isRequired
-		),
-		todosLoaded: PropTypes.bool,
-	};
 	constructor(props) {
 		super(props);
 		this.state = {
