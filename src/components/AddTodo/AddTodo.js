f62a0968154b9ec87aa97e2b775455cc4bd31d18
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-12 19:09:50 +0200

    Implement the controlled field for AddTodo component

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -3,8 +3,21 @@ import PropTypes from "prop-types";
 
 import "./AddTodo.css";
 
-const AddTodo = (props) => <p></p>;
+const AddTodo = ({ addTodoValue, changeValue }) => (
+	<div className="add-todo">
+		<input
+			className="add-todo-input"
+			type="text"
+			value={addTodoValue}
+			onChange={(e) => changeValue(e)}
+		/>
+		<button className="add-todo-btn">+</button>
+	</div>
+);
 
-AddTodo.propTypes = {};
+AddTodo.propTypes = {
+	addTodoValue: PropTypes.string.isRequired,
+	changeValue: PropTypes.func.isRequired,
+};
 
 export default AddTodo;
