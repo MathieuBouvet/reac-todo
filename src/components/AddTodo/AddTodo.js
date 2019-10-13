bc24423a6dbea7c97f95d1e30b0efd478d6960a7
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-13 19:29:36 +0200

    Reformat using prettier

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -4,23 +4,23 @@ import PropTypes from "prop-types";
 import "./AddTodo.css";
 
 const AddTodo = ({ addTodoValue, changeValue, addButtonClick }) => (
-	<div className="add-todo">
-		<input
-			className="add-todo-input"
-			type="text"
-			value={addTodoValue}
-			onChange={(e) => changeValue(e)}
-		/>
-		<button className="add-todo-btn" onClick={(e) => addButtonClick(e)}>
-			＋
-		</button>
-	</div>
+  <div className="add-todo">
+    <input
+      className="add-todo-input"
+      type="text"
+      value={addTodoValue}
+      onChange={(e) => changeValue(e)}
+    />
+    <button className="add-todo-btn" onClick={(e) => addButtonClick(e)}>
+      ＋
+    </button>
+  </div>
 );
 
 AddTodo.propTypes = {
-	addTodoValue: PropTypes.string.isRequired,
-	changeValue: PropTypes.func.isRequired,
-	addButtonClick: PropTypes.func.isRequired,
+  addTodoValue: PropTypes.string.isRequired,
+  changeValue: PropTypes.func.isRequired,
+  addButtonClick: PropTypes.func.isRequired
 };
 
 export default AddTodo;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -10,90 +10,90 @@ import AddTodo from "../AddTodo";
 import data from "../../utils/mockTodo";
 
 class TodoList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			todos: [],
-			todosLoaded: false,
-			addTodoValue: "",
-		};
-	}
-	componentDidMount() {
-		// TODO : consume API
-		setTimeout(() => {
-			this.setState({
-				todos: data,
-				todosLoaded: true,
-			});
-		}, 2500);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      todosLoaded: false,
+      addTodoValue: ""
+    };
+  }
+  componentDidMount() {
+    // TODO : consume API
+    setTimeout(() => {
+      this.setState({
+        todos: data,
+        todosLoaded: true
+      });
+    }, 2500);
+  }
 
-	doneTodoClick = (e, todoId, type) => {
-		e.preventDefault();
-		e.stopPropagation();
-		if (type === "done") {
-			this.setState({
-				todos: this.state.todos.map((elem) => {
-					if (elem._id === todoId) {
-						elem.done = true;
-					}
-					return elem;
-				}),
-			});
-		} else if (type === "delete") {
-			this.setState({
-				todos: this.state.todos.filter((elem) => elem._id !== todoId),
-			});
-		}
-	};
+  doneTodoClick = (e, todoId, type) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (type === "done") {
+      this.setState({
+        todos: this.state.todos.map((elem) => {
+          if (elem._id === todoId) {
+            elem.done = true;
+          }
+          return elem;
+        })
+      });
+    } else if (type === "delete") {
+      this.setState({
+        todos: this.state.todos.filter((elem) => elem._id !== todoId)
+      });
+    }
+  };
 
-	addTodoValueChange = (e) => {
-		this.setState({
-			addTodoValue: e.target.value,
-		});
-	};
+  addTodoValueChange = (e) => {
+    this.setState({
+      addTodoValue: e.target.value
+    });
+  };
 
-	createTodo = (e) => {
-		this.setState({
-			todos: [
-				...this.state.todos,
-				{
-					_id: shortid.generate(),
-					text: this.state.addTodoValue,
-					done: false,
-				},
-			],
-			addTodoValue: "",
-		});
-	};
+  createTodo = (e) => {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          _id: shortid.generate(),
+          text: this.state.addTodoValue,
+          done: false
+        }
+      ],
+      addTodoValue: ""
+    });
+  };
 
-	render() {
-		return !this.state.todosLoaded ? (
-			<div className="loading">
-				<img className="loading-logo" src={logo} alt="loading" />
-				<p> Loading...</p>
-			</div>
-		) : (
-			<React.Fragment>
-				<ul className="todos-loaded">
-					{this.state.todos.map((todo) => (
-						<Todo
-							key={todo._id}
-							id={todo._id}
-							text={todo.text}
-							done={todo.done}
-							clicksTodo={this.doneTodoClick}
-						/>
-					))}
-				</ul>
-				<AddTodo
-					addTodoValue={this.state.addTodoValue}
-					changeValue={this.addTodoValueChange}
-					addButtonClick={this.createTodo}
-				/>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return !this.state.todosLoaded ? (
+      <div className="loading">
+        <img className="loading-logo" src={logo} alt="loading" />
+        <p> Loading...</p>
+      </div>
+    ) : (
+      <React.Fragment>
+        <ul className="todos-loaded">
+          {this.state.todos.map((todo) => (
+            <Todo
+              key={todo._id}
+              id={todo._id}
+              text={todo.text}
+              done={todo.done}
+              clicksTodo={this.doneTodoClick}
+            />
+          ))}
+        </ul>
+        <AddTodo
+          addTodoValue={this.state.addTodoValue}
+          changeValue={this.addTodoValueChange}
+          addButtonClick={this.createTodo}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default TodoList;
