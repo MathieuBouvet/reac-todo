f0af4bafec8d4dc8088869cbdddba00cedee5f35
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-15 14:42:12 +0200

    use State in Todo.js to control leaving animation

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./Todo.css";
 
-const Todo = ({ id, text, done, clicksTodo }) => (
-	<li
-		className={`todo ${done ? "done" : ""}`}
-		onClick={(e) => clicksTodo(e, id, "done")}
-	>
-		<button className="done-todo-btn">{`${done ? "✓" : " "}`}</button>
-		<span className="todo-text">{text}</span>
-		<button
-			className="delete-todo-btn"
-			onClick={(e) => clicksTodo(e, id, "delete")}
-		>
-			✗
-		</button>
-	</li>
-);
+const Todo = ({ id, text, done, clicksTodo }) => {
+  const [willBeRemoved, setWillBeRemoved] = useState(false);
+  return (
+    <li
+      className={`todo ${done ? "done" : ""} ${
+        willBeRemoved ? "will-be-removed" : ""
+      }`}
+      onClick={(e) => clicksTodo(e, id, "done")}
+      onAnimationEnd={(e) => {
+        if (willBeRemoved && e.animationName === "leave-slide") {
+          clicksTodo(e, id, "delete");
+        }
+      }}
+    >
+      <button className="done-todo-btn">{`${done ? "✓" : " "}`}</button>
+      <span className="todo-text">{text}</span>
+      <button
+        className="delete-todo-btn"
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          setWillBeRemoved(true);
+        }}
+      >
+        ✗
+      </button>
+    </li>
+  );
+};
 Todo.propTypes = {
   id: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
