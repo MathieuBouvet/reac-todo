478aea5eb598bb6ec5015c501365bb087b382550
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-30 15:23:04 +0100

    Add the Menu to the app header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import TodoList from "../../components/TodoList";
+import Menu from "./Menu";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> Todo </header>
+      <header className="App-header">
+        Todo <Menu />
+      </header>
       <main className="App-body">
         <TodoList />
       </main>
