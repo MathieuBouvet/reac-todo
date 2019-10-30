992ef4ff3cca2bf8892c2a0a9711978aa7d22ffe
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-30 16:22:10 +0100

    Enable changing userForm values via useState

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import UserForm from "../UserForm";
-const SignIn = () => (
-  <form method="POST" action="">
-    <UserForm username="" password="" />
-  </form>
-);
+const SignIn = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleChange = (event, from) => {
+    if (from === "username") {
+      setUsername(event.target.value);
+    }
+    if (from === "password") {
+      setPassword(event.target.value);
+    }
+  };
+  return (
+    <form method="POST" action="">
+      <UserForm
+        username={username}
+        password={password}
+        changeHandler={handleChange}
+      />
+    </form>
+  );
+};
 
 export default SignIn;
--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,16 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const UserForm = ({ username, password }) => (
+const UserForm = ({ username, password, changeHandler }) => (
   <>
     <label htmlFor="username">Username :</label>
-    <input type="text" name="username" id="username" value={username} />
+    <input
+      type="text"
+      name="username"
+      id="username"
+      value={username}
+      onChange={e => changeHandler(e, "username")}
+    />
     <label htmlFor="password">Password :</label>
-    <input type="password" name="password" id="password" value={password} />
+    <input
+      type="password"
+      name="password"
+      id="password"
+      value={password}
+      onChange={e => changeHandler(e, "password")}
+    />
   </>
 );
 UserForm.propTypes = {
   username: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
+  changeHandler: PropTypes.func.isRequired,
 };
 export default UserForm;
