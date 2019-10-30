897e2ad0dd0210a7dc4fc591bde8f53052a1ae01
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-30 16:11:11 +0100

    Create the SignIn component

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,9 @@
+import React from "react";
+import UserForm from "../UserForm";
+const SignIn = () => (
+  <form method="POST" action="">
+    <UserForm username="" password="" />
+  </form>
+);
+
+export default SignIn;
