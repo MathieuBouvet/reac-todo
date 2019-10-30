2a0743f08cc53925f6bcbe7e81cc7df81c41067f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-30 15:35:19 +0100

    Add space to menu-item

--- src/components/App/Menu.js
+++ src/components/App/Menu.js
@@ -5,10 +5,10 @@ import "./Menu.css";
 
 const Menu = () => (
   <ul className="app-menu">
-    <li>
+    <li className="menu-item">
       <Link to="login">Sign In</Link>
     </li>
-    <li>
+    <li className="menu-item">
       <Link to="signup">Sign Up</Link>
     </li>
   </ul>
