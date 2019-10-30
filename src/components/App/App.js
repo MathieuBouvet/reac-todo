060436be2e4342686ccc296f6ffb83da28a4c64f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-10-30 15:39:07 +0100

    Wrap app title in a link to home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import TodoList from "../../components/TodoList";
 import Menu from "./Menu";
+import { Link } from "@reach/router";
 
 import "./App.css";
 
@@ -9,7 +10,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Todo <Menu />
+        <Link to="/" className="app-title">
+          Todo
+        </Link>
+        <Menu />
       </header>
       <main className="App-body">
         <TodoList />
