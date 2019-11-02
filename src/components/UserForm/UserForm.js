df53d2f11aa891841247acd763bca77bb6d1fdcd
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-02 12:24:17 +0100

    Add has-error class when a form group has error

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,16 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+function groupHasError(fieldName, errors, touched) {
+  return errors[fieldName] && touched[fieldName];
+}
+
 const UserForm = ({ usernameFields, passwordFields, errors, touched }) => (
   <>
-    <div className="form-group">
+    <div
+      className={`form-group ${
+        groupHasError("username", errors, touched) ? "has-error" : ""
+      }`}
+    >
       <label htmlFor="username">Username :</label>
       <input type="text" id="username" {...usernameFields} />
       {touched.username && errors.username && (
         <div className="form-group-error">{errors.username}</div>
       )}
     </div>
-    <div className="form-group">
+    <div
+      className={`form-group ${
+        groupHasError("password", errors, touched) ? "has-error" : ""
+      }`}
+    >
       <label htmlFor="password">Password :</label>
       <input type="password" id="password" {...passwordFields} />
       {touched.password && errors.password && (
