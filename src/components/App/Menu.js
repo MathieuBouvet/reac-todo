c7c9195a79ee349236669dc5098445ae9b79e1f6
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-03 18:03:22 +0100

    Use my Button on app menu

--- src/components/App/Menu.js
+++ src/components/App/Menu.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "@reach/router";
 
+import Button from "../Button";
+
 import "./Menu.css";
 
 const Menu = () => (
   <ul className="app-menu">
     <li className="menu-item">
       <Link to="login">
-        <button>Sign In</button>
+        <Button primaryColor="white">Sign In</Button>
       </Link>
     </li>
     <li className="menu-item">
       <Link to="signup">
-        <button>Sign Up</button>
+        <Button primaryColor="white">Sign Up</Button>
       </Link>
     </li>
   </ul>
