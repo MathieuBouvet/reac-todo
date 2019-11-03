03f85bf26a4492c0afbf6413d869e8d5fec5bfeb
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-04 00:32:24 +0100

    Spin the Spinner

---
+++ src/components/SpinnerButton/Spinner.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { FaSpinner } from "react-icons/fa";
+import "./Spinner.css";
+
+const Spinner = () => <FaSpinner className="button-spinner" />;
+
+export default Spinner;
--- src/components/SpinnerButton/SpinnerButton.js
+++ src/components/SpinnerButton/SpinnerButton.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Button from "../Button";
+import Spinner from "./Spinner";
 
-const SpinnerButton = props => <Button>todo Spinner</Button>;
+const SpinnerButton = ({ spin, children, ...rest }) => (
+  <Button {...rest}>
+    {
+      <>
+        {spin && <Spinner />}
+        {children}
+      </>
+    }
+  </Button>
+);
 
 export default SpinnerButton;
