0a45c4e93b2ac872e60f08860b59378b6cec3f93
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-03 19:17:06 +0100

    Expand on SignUp form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useFormik } from "formik";
+import UserForm from "../UserForm";
 
 const SignUp = () => {
-  return <form></form>;
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  return (
+    <form className="form form-vertical" autoComplete="off">
+      <UserForm
+        usernameFields={formik.getFieldProps("username")}
+        passwordFields={formik.getFieldProps("password")}
+        errors={formik.errors}
+        touched={formik.touched}
+      />
+      <div className="form-group">
+        <label htmlFor="confirmPassword">Confirm password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          {...formik.getFieldProps("confirmPassword")}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SignUp;
