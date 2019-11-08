f4876dcce6f955e0b5faebbb34f19c5234f75039
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-08 21:59:48 +0100

    Use shiny new hook sendingRequest in SignIn component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -2,23 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useFormik } from "formik";
 import UserForm from "../UserForm";
-import useSendingForm from "../../hooks/useSendingForm";
 import validateUser from "../../utils/validateUser";
 import ResponseNotification from "../ResponseNotification";
 import SpinnerButton from "../SpinnerButton";
+import useSendingRequest from "../../hooks/useSendingRequest";
 
 const SignIn = ({ signInSuccessHandler }) => {
-  const formSending = useSendingForm(
-    "http://localhost:3001/api/users/login",
-    signInSuccessHandler
-  );
+  const formSending = useSendingRequest()
+    .post("http://localhost:3001/api/users/login")
+    .onSuccess(signInSuccessHandler);
   const formik = useFormik({
     initialValues: {
       username: "",
       password: "",
     },
     validate: validateUser,
-    onSubmit: formSending.submitHandler,
+    onSubmit: formSending.send,
   });
   return (
     <form
@@ -32,13 +31,13 @@ const SignIn = ({ signInSuccessHandler }) => {
         touched={formik.touched}
       />
       <SpinnerButton
-        spin={formSending.sending}
+        spin={formSending.loading}
         type="submit"
         className="submit-login"
       >
         Log In
       </SpinnerButton>
-      {formSending.sent && (
+      {formSending.done && (
         <ResponseNotification
           error={formSending.error}
           closeHandler={formSending.reset}
