379ec9a9173f8c1ece04c510830eab52a294b53c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-08 21:11:30 +0100

    Refactor the hook to be handled with methods

--- src/hooks/useSendingRequest.js
+++ src/hooks/useSendingRequest.js
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import axios from "axios";
 
-function useSendingRequest(method, url, onSuccess, onError) {
+function useSendingRequest() {
+  let requestConfig = {
+    method: "get",
+    url: "",
+    timeout: 5000,
+    headers: {},
+  };
+  let onSuccess = null;
+  let onError = null;
   const [requestState, setRequestState] = useState({
     loading: false,
     error: null,
@@ -9,13 +17,9 @@ function useSendingRequest(method, url, onSuccess, onError) {
   });
 
   const sendRequest = data => {
+    const config = { ...requestConfig, data };
     setRequestState({ loading: true, error: null, done: false });
-    axios({
-      method,
-      url,
-      data,
-      timeout: 5000,
-    })
+    axios(config)
       .then(response => {
         setRequestState({ loading: false, error: null, done: true });
         if (onSuccess) {
@@ -30,7 +34,54 @@ function useSendingRequest(method, url, onSuccess, onError) {
       });
   };
 
-  return { ...requestState, sendRequest };
+  return {
+    ...requestState,
+    sendRequest,
+    get(url) {
+      requestConfig.method = "get";
+      requestConfig.url = url;
+      return this;
+    },
+    post(url) {
+      requestConfig.method = "post";
+      requestConfig.url = url;
+      return this;
+    },
+    put(url) {
+      requestConfig.method = "put";
+      requestConfig.url = url;
+      return this;
+    },
+    delete(url) {
+      requestConfig.method = "delete";
+      requestConfig.url = url;
+      return this;
+    },
+    bearer(token) {
+      requestConfig.headers = {
+        ...requestConfig.headers,
+        Authorization: `Bearer ${token}`,
+      };
+      return this;
+    },
+    onSuccess(callback) {
+      onSuccess = callback;
+      return this;
+    },
+    onError(callback) {
+      onError = callback;
+      return this;
+    },
+    config(config) {
+      const headers = { ...requestConfig.headers, ...config.headers };
+      requestConfig = {
+        ...requestConfig,
+        ...config,
+        headers,
+      };
+      return this;
+    },
+  };
 }
 
 export default useSendingRequest;
