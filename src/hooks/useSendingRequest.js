69b0cb0f4aae83a46f05d0582b7052150ff421f3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-08 14:58:02 +0100

    Create the state of a request

--- src/hooks/useSendingRequest.js
+++ src/hooks/useSendingRequest.js
@@ -1,4 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function() {}
+export default function() {
+  const [requestState, setRequestState] = useState({
+    loading: false,
+    error: null,
+    done: false,
+  });
+}
