84f4601b9a7429e4878dd4c02783a3eda91b2d87
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-08 15:19:48 +0100

    Name the hook

--- src/hooks/useSendingRequest.js
+++ src/hooks/useSendingRequest.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function() {
+function useSendingRequest(method, url) {
   const [requestState, setRequestState] = useState({
     loading: false,
     error: null,
     done: false,
   });
 }
+
+export default useSendingRequest;
