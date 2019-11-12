4e0d1d1de6a4f59083a6a9963043ad65280ca947
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-12 16:21:38 +0100

    Display todos item

--- src/components/TodoList/TodoListDummy.js
+++ src/components/TodoList/TodoListDummy.js
@@ -1,7 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const TodoListDummy = ({ todoList }) => <div></div>;
+import Todo from "../Todo";
+import "./TodoList.css";
+
+const TodoListDummy = ({ todoList }) => (
+  <div className="todo-list">
+    {!todoList.length && "Rien à faire ;)"}
+    <ul className="todos-loaded">
+      {todoList.map(todoItem => (
+        <Todo {...todoItem} />
+      ))}
+    </ul>
+  </div>
+);
 
 TodoListDummy.propTypes = {
   todoList: PropTypes.arrayOf(
