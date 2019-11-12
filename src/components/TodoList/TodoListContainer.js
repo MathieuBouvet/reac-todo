2581cbe960132bf61fa859431bfc821d485cc4da
Mathieu Bouvet <mathieubouvet42@gmail.com>
2019-11-12 17:08:21 +0100

    Implement an add todo

--- src/components/TodoList/TodoListContainer.js
+++ src/components/TodoList/TodoListContainer.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
+import shortid from "shortid";
+
 import TodoList from "./TodoListDummy";
 
+const todoListReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        { tempId: shortid.generate(), text: action.text, done: false },
+      ];
+    default:
+      return state;
+  }
+};
+
 const TodoListContainer = () => {
-  return <TodoList todoList={[]} />;
+  const [todoList, dispatch] = useReducer(
+    todoListReducer,
+    JSON.parse(window.localStorage.getItem("todoList")) || []
+  );
+
+  return <TodoList todoList={todoList} dispatch={dispatch} />;
 };
 
 export default TodoListContainer;
